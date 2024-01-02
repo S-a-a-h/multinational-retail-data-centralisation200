be11ce2131bbd9d4790f542d27f326148a2f7891
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-02 22:24:24 +0000

    creates shells of methods: clean_user_data and upload_to_db

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,17 @@
 #Class with methods to clean data from each of the data sources
 
 class DataCleaning:
+#Initialize:
+    def __init__ ():
+
+#Clean the user data, look out for NULL values, errors with dates, incorrectly typed values and rows filled with the wrong information
+
+#Handling NULL values: Replace NULLs with a default value, impute using statistics, or drop rows/columns with too many missing values.
+#Correcting date errors: Standardize date formats, parse and fill missing or incorrect dates.
+#Correcting incorrectly typed values: Convert data types to the appropriate format (e.g., dates to datetime objects, strings to numeric values).
+#Handling rows with wrong information: Identify outliers or incorrect entries and remove or correct them.
+    def clean_user_data(self):
+        
+
+#This method will take in a Pandas DataFrame and table name to upload to as an argument.
+    def upload_to_db():
