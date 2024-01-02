04555cc2bb4f547f69b896748c33e437a0b4df3d
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-02 21:55:36 +0000

    adds init method

--- database_utils.py
+++ database_utils.py
@@ -4,23 +4,27 @@ from sqlalchemy import create_engine, inspect
 
 
 class DatabaseConnector:
+#initialize the db engine (makes connecting to other classes smoother)
+    def __init__(self, creds_path):
+        self.creds_path = creds_path
+        self.engine = self.init_db_engine()
+
 #Create a method read_db_creds this will read the credentials yaml file and return a dictionary of the credentials.
-    def read_db_creds(self, creds_path):
-        with open(creds_path, 'r') as creds:
+    def read_db_creds(self):
+        with open(self.creds_path, 'r') as creds:
             credentials = yaml.safe_load(creds)
         return dict(credentials)
 
 #read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine
-    def init_db_engine(self, creds_path):
-        credentials = self.read_db_creds(creds_path)
+    def init_db_engine(self):
+        credentials = self.read_db_creds()
         db_url = f"postgresql://{credentials['RDS_USER']}:{credentials['RDS_PASSWORD']}@{credentials['RDS_HOST']}:{credentials['RDS_PORT']}/{credentials['RDS_DATABASE']}"
         engine = create_engine(db_url)
         return engine
 
 #Using the engine from init_db_engine create a method list_db_tables to list all the tables in the database so you know which tables you can extract data from.
-    def list_db_tables(self, creds_path):
-        engine = self.init_db_engine(creds_path)
-        inspector = inspect(engine)
+    def list_db_tables(self):
+        inspector = inspect(self.engine)
         table_names = inspector.get_table_names()
         return table_names
         
