fa83a861c5a934e7752b63ad44384be6a369d256
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-05 16:01:15 +0000

    More tips on future milestones

--- data_transform.py
+++ data_transform.py
@@ -11,5 +11,10 @@
 #So a lot of what's wrong in one table might be the same thing in another table
 #Also don't worry about the types too much here as milestone 3 asks you to change the types
 
+#Other tips as well categorical columns are great to target with df["column_name"].unique()
+#As they don't have many values so if there are errors with their rows they'll show up in the unique list of values
+#Also in a later milestone you're going to be connecting some tables together with a relationship
+#If that relationship doesn't work then you know there's something wrong with the cleaning
+
 class DataTransformer:
     
