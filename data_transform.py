ee7e584a4874ae9399b05bc9656c0ab887cba272
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-06 17:09:19 +0000

    updates instructions for this class creation

--- data_transform.py
+++ data_transform.py
@@ -1,14 +1,5 @@
-#I would have another class called DataTransformer or DataTransform something like that
-#Then I would create all the methods that clean data inside that class
-#I would then import that class into the script that has DataExtractor inside it
-#When creating the clean_user_data method I would then have something like this
-
-#def clean_user_data():
-#   DataTransform.convert_col_dtype(column="Name", type="str")
-#  DataTransform.clean_weights(column_name="weights")
-
-#Then you're clean_user_data is going to look super readable and clear in what it's trying to achieve
-#So a lot of what's wrong in one table might be the same thing in another table
+#Create all cleaning processes in this class 
+#As a lot of what's wrong in one table might be the same thing in another table
 #Also don't worry about the types too much here as milestone 3 asks you to change the types
 
 #Other tips as well categorical columns are great to target with df["column_name"].unique()
