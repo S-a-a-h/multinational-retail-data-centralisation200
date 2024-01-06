6d095fc4daebb555fad057603a3431c3b6194cc1
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-06 16:58:44 +0000

    sets up the DataCleaning class

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,24 +1,10 @@
 #Class with methods to clean data from each of the data sources
 import pandas as pd
-from database_utils import DatabaseConnector
-from data_extraction import DataExtractor
-from data_transform import DataTransformer
+
 
 class DataCleaning:
-#Initialize:
-    def __init__ (self, creds_path):
-        self.connector_instance = DatabaseConnector(creds_path)
-        self.extractor_instance = DataExtractor()
-
-#Clean the user data, look out for NULL values, errors with dates, incorrectly typed values and rows filled with the wrong information
-
-#Handling NULL values: Replace NULLs with a default value, impute using statistics, or drop rows/columns with too many missing values.
-#Correcting date errors: Standardize date formats, parse and fill missing or incorrect dates.
-#Correcting incorrectly typed values: Convert data types to the appropriate format (e.g., dates to datetime objects, strings to numeric values).
-#Handling rows with wrong information: Identify outliers or incorrect entries and remove or correct them.
-    def clean_user_data(self):
-        DataTransformer.convert_col_dtype(column="Name", type="str")
-        DataTransformer.clean_weights(column_name="weights")
+    #DataTransformer.convert_col_dtype(column="Name", type="str")
+    #DataTransformer.clean_weights(column_name="weights")
 
         
 
