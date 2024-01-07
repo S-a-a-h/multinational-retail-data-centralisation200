bc77af25e368b32c5820b39677574f0a8c87ec19
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-07 13:36:02 +0000

    WIP identifies methods to perform on df

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,7 +4,12 @@ import pandas as pd
 
 class DataCleaning:
     #DataTransformer.convert_col_dtype(column="Name", type="str")
-    #DataTransformer.clean_weights(column_name="weights")
+    #DataTransformer.clean_weights(column_name="weights") 
+    #syntax: class.cleaning_method_from_class(column_to_clean_from_df)
+
+    #drop null rows with df.dropna()
+    #drop duplicate rows in whole df
+    #drop incorrect data rows
 
         
 
