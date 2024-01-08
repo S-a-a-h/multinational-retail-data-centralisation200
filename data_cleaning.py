f317ecbe27ecb09dea2a6fb39dcbdd870d8e67df
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-08 13:08:01 +0000

    WIP - CLEANING METHODS NOT WORKING

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,9 +7,24 @@ class DataCleaning:
     #DataTransformer.clean_weights(column_name="weights") 
     #syntax: class.cleaning_method_from_class(column_to_clean_from_df)
 
-    #drop null rows with df.dropna()
-    #drop duplicate rows in whole df
-    #drop incorrect data rows
+    @staticmethod
+    def clean_store_data(legacy_store_details):
+        #legacy_store_details['country_code'].unique()
+        legacy_store_details = legacy_store_details.drop(legacy_store_details[~legacy_store_details['country_code'].isin(['GB', 'US', 'DE'])].index)
+        return legacy_store_details
+
+
+    @staticmethod
+    def hello():
+        print('hello world')
+
+    #def clean_users_data(legacy_users_df):
+
+
+
+
+    #def clean_orders_data(orders_table_df):
+        
 
         
 
