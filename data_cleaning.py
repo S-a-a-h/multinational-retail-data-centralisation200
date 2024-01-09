861129ddc73206cedad5845fc2b35e71306c7a6b
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-09 19:41:33 +0000

    removes all methods to use data_processing class

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,5 @@
 #Class with methods to clean data from each of the data sources
+#from data_transform import DataTransformer
 import pandas as pd
 
 
@@ -7,29 +8,13 @@ class DataCleaning:
     #DataTransformer.clean_weights(column_name="weights") 
     #syntax: class.cleaning_method_from_class(column_to_clean_from_df)
 
-    @staticmethod
-    def clean_store_data(legacy_store_details):
-        #legacy_store_details['country_code'].unique()
-        legacy_store_details = legacy_store_details.drop(legacy_store_details[~legacy_store_details['country_code'].isin(['GB', 'US', 'DE'])].index)
-        return legacy_store_details
+    #clean store_df
 
 
-    @staticmethod
-    def hello():
-        print('hello world')
-
-#from data_transform import DataTransformer
-
-#transform = DataTransformer()
-#columns_to_process = ['latitude', 'staff_numbers', 'longitude']
-#store_df = transform.convert_and_drop(store_df, columns_to_process)
-
-    #def clean_users_data(legacy_users_df):
+    #clean users_df
 
 
-
-
-    #def clean_orders_data(orders_table_df):
+    #clean orders_df
         
 
         
