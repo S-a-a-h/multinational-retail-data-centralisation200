19155657a31dc6cd4ca86edcd51e12ae6d018ebe
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-10 23:17:46 +0000

    removes comment

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,3 @@
-#This class will work as a utility class, in it you will be creating methods that help extract data from different data sources.
-#The methods contained will be fit to extract data from a particular data source, these sources will include CSV files, an API and an S3 bucket.
 import pandas as pd
 from database_utils import DatabaseConnector
 
