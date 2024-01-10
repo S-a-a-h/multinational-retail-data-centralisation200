6863f25916b72764f17e324b81f70c8a65c86f5f
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-10 23:17:10 +0000

    no changes made

--- data_extraction.py
+++ data_extraction.py
@@ -4,13 +4,11 @@ import pandas as pd
 from database_utils import DatabaseConnector
 
 class DataExtractor:
-
     def __init__(self, database_connector):
         self.database_connector = database_connector
     
     def read_rds_table(self, table_name):
         tables = self.database_connector.list_db_tables()
-        
         if table_name in tables:
             engine = self.database_connector.engine
             return pd.read_sql_table(table_name, con=engine)
