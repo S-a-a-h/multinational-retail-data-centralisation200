e46982d55cbe392b8e655da357a1d45a8c7e19f8
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-10 01:12:06 +0000

    no changes

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 class DataCleaning:
+    
     #DataTransformer.convert_col_dtype(column="Name", type="str")
     #DataTransformer.clean_weights(column_name="weights") 
     #syntax: class.cleaning_method_from_class(column_to_clean_from_df)
