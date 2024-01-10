7fe919ab3817c468a4dd2075d30954071c291102
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-10 16:27:22 +0000

    organized staticmethods and all methods for store_df

--- data_processing.py
+++ data_processing.py
@@ -43,103 +43,95 @@ class DataProcessor:
 
 
     #All dfs - remember to assign each df to the methods being used in turn inorder to process and update them  
-    @staticmethod
-    def clean_address(df, column_name):
-        store_address = store_df['address'].str.split('\n', expand=True) #removes \n and splits the address into sections
-        store_address.columns = ['street', 'city', 'postal_code', 'other_details']
-        store_df = pd.concat([store_df, store_address], axis=1)
-        store_df.drop(columns=['address'], inplace=True)
-        return store_df
-    #change the internal code of this method to static method!
-
-    @staticmethod
-    def drop_rows_with_numeric(df, columns): 
-        pattern = re.compile(r'\d')
-        for col in columns:
-            df = df[~df[col].astype(str).str.contains(pattern, na=False)]
-        return df
-    #use this method to filter:(users_df, 'first_name', 'last_name')
-    #use this method to filter:(orders_df, 'first_name', 'last_name')
-
-    @staticmethod
-    def clean_uuids(df, column_name):
-        uuid_pattern = re.compile(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
-        df[column_name] = df[column_name].apply(lambda x: x if uuid_pattern.match(str(x)) else None)
-        return df
-    #(users_df, 'user_uuid')
-    #(orders_df, 'user_uuid', 'date_uuid')
-
-    #numeric columns method to convert dt then drop NaN values in the columns
-    @staticmethod
-    def convert_and_drop(df, column_names):
-        for column_name in column_names:
-            df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
-            df.dropna(subset=[column_name], inplace=True) #drops NaN values
-        return df
         
+    #method to drop duplicates in a df
     @staticmethod
     def drop_duplicates(df):
         df = df.drop_duplicates()
         return df
     
     @staticmethod
+    def remove_invalid_values(df, column_names):
+        pattern = re.compile(r'^[A-Za-z0-9]{10}$')
+        for column_name in column_names:
+            if column_name in df.columns:
+                df = df[~(df[column_name].astype(str).str.match(pattern, na=False) | df[column_name].isnull())]
+        return df
+    #(store_df, ['locality', 'store_code'])
+
+    #method to convert dt to numeric then drop NaN values in those numeric columns
+    @staticmethod
+    def convert_and_drop_non_numeric(df, column_names):
+        for column_name in column_names:
+            df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
+            df.dropna(subset=[column_name], inplace=True) #drops NaN values
+        return df
+    #(store_df, ['latitude', 'staff_numbers', 'longitude'])
+
+    @staticmethod
+    def clean_address(df, column_name):
+        df[column_name] = df[column_name].str.replace('\n', '', regex=True)
+        df.dropna(subset=[column_name], inplace=True)
+        pattern = re.compile(r'^[A-Za-z0-9]{10}$')
+        df = df[~df[column_name].str.match(pattern, na=False)]
+        return df
+    #(store_df, 'address')
+    #(users_df, 'address')  
+
+    @staticmethod
+    def drop_rows_with_numeric(df, column_names): 
+        pattern = re.compile(r'\d')
+        for col in column_names:
+            df = df[~df[col].astype(str).str.contains(pattern, na=False)]
+        return df
+    #(users_df, ['first_name', 'last_name'])
+    #(orders_df, ['first_name', 'last_name'])
+
+    @staticmethod
+    def clean_uuids(df, column_names):
+        uuid_pattern = re.compile(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
+        for column_name in column_names:
+            if column_name in df.columns:
+                df[column_name] = df[column_name].apply(lambda x: x if uuid_pattern.match(str(x)) else None)
+        return df
+    #(users_df, ['user_uuid'])
+    #(orders_df, ['user_uuid', 'date_uuid'])
+    
+    @staticmethod
     def drop_df_cols(df, column_names):
         df = df.drop(columns=column_names, inplace=True)
-    #(store_df, 'lat')
-    #(orders_df, 'level_0', '1')
+    #(store_df, ['lat'])
+    #(orders_df, ['level_0', '1'])
     
+    @staticmethod
+    def remove_invalid_dates(df, column_names):
+        date_pattern = r'^\d{4}-\d{2}-\d{2}$'
+        for column_name in column_names:
+            if column_name in df.columns:
+                df[column_name] = pd.to_datetime(df[column_name], errors='coerce')
+                df = df[df[column_name].notnull() & df[column_name].astype(str).str.match(date_pattern, na=False)]
+        return df
+    #(store_df, ['opening_date'])
+    #(users_df, ['join_date', 'date_of_birth'])
+
+    @staticmethod
+    def clean_fnames_lnames(df):
+        pattern = re.compile(r'\d') 
+        if 'first_name' in df.columns and 'last_name' in df.columns:
+            df = df.loc[~(df['first_name'].astype(str).str.contains(pattern, na=False) | df['first_name'].isnull())]
+            df = df.loc[~(df['last_name'].astype(str).str.contains(pattern, na=False) | df['last_name'].isnull())]
+        return df
+    #(users_df)
+    #(order_df)
+
+
+
+
+    #final method to correct index column
     @staticmethod
     def fix_index(df, index_col):
         df.set_index(index_col, inplace=True)
-    
-    #WIP
-    @staticmethod
-    def remove_invalid_dates(df, column_names):
-
-
-    def clean_store_odate(self, store_df):
-        odate_to_remove = [ 'NULL', 'ZCXWWKF45G', '7AHXLXIUEF', '0OLAK2I6NS', 'A3PMVM800J', 'GMMB02LA9V', '13PIY8GD1H', '36IIMAQD58']
-        store_df = store_df[~store_df['opening_date'].isin(odate_to_remove)]
-        store_df['opening_dates'] = store_df['opening_dates'].apply(lambda x: pd.to_datetime(x, errors='coerce').strftime('%Y-%m-%d') if pd.notnull(pd.to_datetime(x, errors='coerce')) else x)
-        return store_df
-    
-    users_df['join_date'].unique()
-    >jdate_pattern = r'^\d{4}-\d{2}-\d{2}$'
-    >non_standard_jdates = users_df[~users_df['join_date'].str.match(jdate_pattern)]
-    >print(non_standard_jdates['join_date'].values) 
-    >jdate_to_remove = ['NULL', 'JJ2PDVNPRO', 'AHN6EKASH3', 'FYF2FAPZF3', 'QH6Z9ZPX37', 'LYVWXBBI6F', 'DM4Q84QZ03', 'DOKMYDVV6L', 'YBUYH8T6OE', 'SRH5SM36LH', '4JIOCHZY0W', ‘3CUODA3HTC', '8BAER2328P', 'U9CRKSTONU', '9YLGYDEZNV', '7PF0SMLXII']
-    >users_df = users_df[~users_df['join_date'].isin(jdate_to_remove)]
-    >users_df['join_date'] = users_df['join_date'].apply(lambda x: pd.to_datetime(x, errors='coerce').strftime('%Y-%m-%d') if pd.notnull(pd.to_datetime(x, errors='coerce')) else x)
-
-    users_df['date_of_birth'].unique()
-    >dob_pattern = r'^\d{4}-\d{2}-\d{2}$'
-    >non_standard_dob = users_df[~users_df['date_of_birth'].str.match(dob_pattern)]
-    >print(non_standard_dob['date_of_birth'].values) 
-    >dob_to_remove = ['NULL', 'KBTI7FI7Y3', 'OFH8YGZJWN', 'PQPEUO937L', '7KGJ3C5TSW', 'RQTF5XSXP4', 'D2OZZHWOLK', 'QTVEU5TR8H', 'L3E8OV4UAC', 'TLSTUEIKI0', 'YTC82GP4XE', 'O1LIA1MT1N', 'RQI3KQXFBQ', '1IA43NTJFB', 'M7HZECVQVC', '6WAQ2SVK0Q']
-    >users_df = users_df[~users_df['date_of_birth'].isin(dob_to_remove)]
-    >users_df['date_of_birth'] = users_df['date_of_birth'].apply(lambda x: pd.to_datetime(x, errors='coerce').strftime('%Y-%m-%d') if pd.notnull(pd.to_datetime(x, errors='coerce')) else x)
-    #create static method to convert dates to datetime and drop all values which does not match the datetime format
-    
-    #WIP
-    @staticmethod
-    def remove_invalid_values(df, column_names):
-    
-
-    def clean_store_locality(self, store_df):
-        locality_to_remove = ['N/A',  'NULL', '9IBH8Y4Z0S', '1T6B406CI8', '6LVWPU1G64', 'RX9TCP2RGB', 'CQMHKI78BX', 'RY6K0AUE7F', '3VHFDNP8ET']
-        store_df = store_df[~store_df['locality'].isin(locality_to_remove)]
-        return store_df
-    
-    def clean_store_code(self, store_df):
-        codes_to_remove = ['NULL', 'NRQKZWJ9OZ', 'QIUU9SVP51', 'Y8J0Z2W8O9', 'ISEE8A57FE', 'T0R2CQBDUS', 'TUOKF5HAAQ', '9D4LK7X4LZ']
-        store_df = store_df[~store_df['store_code'].isin(codes_to_remove)]
-        return store_df
-    #create static method to drop all values which does not match the invlaid format (make a pattern) or is a NULL or N/A value
-    
-    #WIP
-    @staticmethod
-    def clean_fnames_lnames(df, column_names):
-        pattern = re.compile(r'\d') 
-        numeric_first_names = users_df[users_df['first_name'].astype(str).str.contains(pattern, na=False)].index
-        numeric_last_names = users_df[users_df['last_name'].astype(str).str.contains(pattern, na=False)].index
-    #edit this method to make it staticmethod
+        df.index = df.index + 1 
+    #(store_df, 'index')
+    #(users_df, 'index')
+    #(orders_df, 'index')
