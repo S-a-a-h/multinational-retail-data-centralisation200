0f5bdf239e067e0acd13bc1f275d34c8b8b0f5e7
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-11 20:00:43 +0000

    attempts to find output source of None

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,5 @@
 from data_processing import DataProcessor
+from IPython.display import display
 import pandas as pd
 
 class DataCleaning(DataProcessor):
@@ -20,17 +21,17 @@ class DataCleaning(DataProcessor):
 
     #USERS_DF
     def clean_users_df(self, users_df):
-        cleaned_users_df = DataProcessor.clean_users_email_address(users_df)
-        cleaned_users_df = DataProcessor.clean_users_country(cleaned_users_df)
-        cleaned_users_df = DataProcessor.clean_users_country_code(cleaned_users_df)
-        cleaned_users_df = DataProcessor.clean_users_company(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.clean_users_email_address(users_df)
+        #cleaned_users_df = DataProcessor.clean_users_country(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.clean_users_country_code(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.clean_users_company(cleaned_users_df)
         cleaned_users_df = DataProcessor.clean_address(cleaned_users_df, 'address')
-        cleaned_users_df = DataProcessor.clean_uuids(cleaned_users_df, ['user_uuid'])
-        cleaned_users_df = DataProcessor.remove_invalid_dates(cleaned_users_df, ['join_date', 'date_of_birth'])
-        cleaned_users_df = DataProcessor.clean_fnames_lnames(cleaned_users_df)
-        cleaned_users_df = DataProcessor.drop_null_values(cleaned_users_df)
-        cleaned_users_df = DataProcessor.drop_duplicates(cleaned_users_df)
-        cleaned_users_df = DataProcessor.fix_index(cleaned_users_df, 'index')
+        #cleaned_users_df = DataProcessor.clean_uuids(cleaned_users_df, ['user_uuid'])
+        #cleaned_users_df = DataProcessor.remove_invalid_dates(cleaned_users_df, ['join_date', 'date_of_birth'])
+        #cleaned_users_df = DataProcessor.clean_fnames_lnames(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.drop_null_values(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.drop_duplicates(cleaned_users_df)
+        #cleaned_users_df = DataProcessor.fix_index(cleaned_users_df, 'index')
         return cleaned_users_df
 
     #ORDERS_DF
