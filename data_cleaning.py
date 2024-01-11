21e626e2bcdeab09d70f3d45c40a37787f39d36e
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-11 00:42:04 +0000

    reorders the DataProcessor methods for linear coding style

--- data_cleaning.py
+++ data_cleaning.py
@@ -13,6 +13,7 @@ class DataCleaning(DataProcessor):
         cleaned_store_df = DataProcessor.clean_address(store_df, 'address')
         cleaned_store_df = DataProcessor.drop_df_cols(store_df, ['lat'])
         cleaned_store_df = DataProcessor.remove_invalid_dates(store_df, ['opening_date'])
+        cleaned_store_df = DataProcessor.drop_null_values(store_df)
         cleaned_store_df = DataProcessor.drop_duplicates(store_df)
         cleaned_store_df = DataProcessor.fix_index(store_df, 'index')
         return cleaned_store_df
@@ -27,6 +28,7 @@ class DataCleaning(DataProcessor):
         cleaned_users_df = DataProcessor.clean_uuids(users_df, ['user_uuid'])
         cleaned_users_df = DataProcessor.remove_invalid_dates(users_df, ['join_date', 'date_of_birth'])
         cleaned_users_df = DataProcessor.clean_fnames_lnames(users_df)
+        cleaned_users_df = DataProcessor.drop_null_values(users_df)
         cleaned_users_df = DataProcessor.drop_duplicates(users_df)
         cleaned_users_df = DataProcessor.fix_index(users_df, 'index')
         return cleaned_users_df
@@ -38,6 +40,7 @@ class DataCleaning(DataProcessor):
         cleaned_orders_df = DataProcessor.clean_fnames_lnames(orders_df)
         cleaned_orders_df = DataProcessor.clean_uuids(orders_df, ['user_uuid', 'date_uuid'])       
         cleaned_orders_df = DataProcessor.drop_df_cols(orders_df,  ['level_0', '1'])
+        cleaned_orders_df = DataProcessor.drop_null_values(orders_df)
         cleaned_orders_df = DataProcessor.drop_duplicates(orders_df)
         cleaned_orders_df = DataProcessor.fix_index(orders_df, 'index')
         return cleaned_orders_df
