eadbd2db982af546bd846ab1aec807fab594f0cd
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-15 18:24:22 +0000

    cleans b_store_df

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,9 @@
 from data_processing import DataProcessor
+import pandas as pd
 
 class DataCleaning(DataProcessor):
     
-    #USERS_DF
+    #USERS_DF (users_df)
     def clean_users_df(self, users_df):
         cleaned_users_df_dup = DataProcessor.drop_duplicates(users_df) #drops
         cleaned_users_df_con = DataProcessor.clean_users_country(cleaned_users_df_dup) #filters and drops
@@ -13,23 +14,25 @@ class DataCleaning(DataProcessor):
         cleaned_users_df = DataProcessor.fix_index(cleaned_users_df_dates, 'index')
         return cleaned_users_df
     
-    #CARD_DF
-    def clean_card_data(self, card_df): # - REORDER
+    #CARD_DF (card_df)
+    def clean_card_data(self, card_df): 
         cleaned_card_df_dup = DataProcessor.drop_duplicates(card_df) #drops
         cleaned_card_df_cprov = DataProcessor.card_prov(cleaned_card_df_dup) #filters only
         cleaned_card_df_dropcols = DataProcessor.drop_df_cols(cleaned_card_df_cprov, ['card_number expiry_date', 'Unnamed: 0']) #drops
         cleaned_card_df_pdates = DataProcessor.clean_dates(cleaned_card_df_dropcols, ['date_payment_confirmed']) #formats and drops
-        cleaned_card_df_edates = DataProcessor.clean_store_edate(cleaned_card_df_pdates, 'expiry_date') #formats and drops
-        cleaned_card_df = DataProcessor.clean_card_number(cleaned_card_df_edates, ['card_number']) #formats and drops
+        cleaned_card_df_edates = DataProcessor.clean_store_edate(cleaned_card_df_pdates) #formats and drops
+        cleaned_card_df = DataProcessor.clean_card_number(cleaned_card_df_edates) #formats and drops
         return cleaned_card_df
-
     
-    #BUSINESS STORE DF
-    def clean_store_data(self, b_store_df):
-        cleaned_b_store_df = DataProcessor.method(b_store_df)
-        cleaned_b_store_df = DataProcessor.method(cleaned_b_store_df)
-        cleaned_b_store_df = DataProcessor.method(cleaned_b_store_df) 
-        cleaned_b_store_df = DataProcessor.method(cleaned_b_store_df) 
+    #BUSINESS STORE DF (b_store_df)
+    def clean_store_data(self, b_store_df): 
+        cleaned_b_store_df_dup = DataProcessor.drop_duplicates(b_store_df)
+        cleaned_b_store_df_add = DataProcessor.clean_address(cleaned_b_store_df_dup, 'address')
+        cleaned_b_store_df_con = DataProcessor.clean_store_continent(cleaned_b_store_df_add) 
+        cleaned_b_store_df_drop = DataProcessor.drop_df_cols(cleaned_b_store_df_con, ['lat'])
+        cleaned_b_store_df_num = DataProcessor.tonumeric_and_drop_non_numeric(cleaned_b_store_df_drop, ['longitude', 'staff_numbers', 'latitude']) 
+        cleaned_b_store_df_odate = DataProcessor.clean_dates(cleaned_b_store_df_num, ['opening_date']) 
+        cleaned_b_store_df = DataProcessor.fix_index(cleaned_b_store_df_odate, 'index') 
         return cleaned_b_store_df
 
 
