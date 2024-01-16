eebb5f8d5acbf99b030b7eda14f55a7c76ec69e2
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-16 01:04:11 +0000

    all methods to extract the datas

--- data_extraction.py
+++ data_extraction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tabula
 import requests
 import boto3
+import json
 
 class DataExtractor:
     def __init__(self, db_connector_instance):
@@ -11,11 +12,11 @@ class DataExtractor:
         self.s3_client = boto3.client('s3')
 
 
-    #USERS_DF
+    #USERS_DF & ORDERS_DF
     def read_rds_table(self, table_name):
-        tables = self.database_connector.list_db_tables()
+        tables = self.db_connector_instance.list_db_tables()
         if table_name in tables:
-            engine = self.database_connector.engine
+            engine = self.db_connector_instance.engine
             return pd.read_sql_table(table_name, con=engine)
 
 
@@ -39,7 +40,7 @@ class DataExtractor:
         response = requests.get(number_of_stores_endpoint, headers=self.header)
         if response.status_code == 200:
             number_of_stores = response.json().get('number_stores')
-            print(f"Number of stores: {number_of_stores}")
+            print(f'Number of stores: {number_of_stores}')
         return int(number_of_stores)
 
     def retrieve_stores_data(self, store_endpoint, number_of_stores):
@@ -73,12 +74,19 @@ class DataExtractor:
 
     #PRODS_DF
     def extract_from_s3(self, s3_address):
-        bucket, key = s3_address.split("://")[1].split("/", 1)
+        bucket, key = s3_address.split('://')[1].split('/', 1)
         response = self.s3_client.get_object(Bucket=bucket, Key=key)
         data = pd.read_csv(response['Body'])
         prods_df = pd.DataFrame(data)
         return prods_df
 
-
+    #SDT_DF
+    def extract_sdt(self, s3_url):
+        response = requests.get(s3_url)
+        if response.status_code == 200:
+            sdt = json.loads(response.text)
+            print('File downloaded successfully.')
+            sdt_df = pd.DataFrame(sdt)
+            return sdt_df
 
 
