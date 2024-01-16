0cb6fecfb88059add801cfe2bb2416f213461963
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-01-16 21:49:19 +0000

    adds docstrings

--- data_processing.py
+++ data_processing.py
@@ -1,79 +1,84 @@
 import pandas as pd
-import numpy as np
 import re
 
 
 class DataProcessor:
+    '''
+    DataProcessor Class
+    -------
+    This script transforms the data by processing, correcting values and dropping any invalid rows from each DataFrame.
+    The methods included are all staticmethods for code reusablity on all DataFrames created in this project.
+    All methods and variables are named descriptively for readability.
+    '''
+
     #USERS_DF METHODS ONLY 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod 
     def clean_users_country(users_df):
         valid_countries = ['Germany', 'United Kingdom', 'United States']
         users_df = users_df[users_df['country'].isin(valid_countries)]
         return users_df
-    #(users_df)
     
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_users_country_code(users_df):
         valid_country_codes = ['GB', 'US', 'DE']
         country_code_mapping = {'GGB': 'GB'}
         users_df.loc[:, 'country_code'] = users_df['country_code'].apply(lambda c_c: country_code_mapping.get(c_c, c_c))
         users_df = users_df[users_df['country_code'].isin(valid_country_codes)]
         return users_df
-    #(users_df)
     
 
     #CARD_DF METHODS ONLY
-    @staticmethod # - CLEANED (not over-engineered)
-    def card_prov(card_df):
+    @staticmethod
+    def clean_card_prov(card_df):
         valid_card_provs = ['Diners Club / Carte Blanche', 'American Express', 'JCB 16 digit', 'JCB 15 digit', 'Maestro', 'Mastercard', 'Discover', 'VISA 19 digit', 'VISA 16 digit', 'VISA 13 digit']
         card_df = card_df[card_df['card_provider'].isin(valid_card_provs)]
         return card_df
-    #(card_df)
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_store_edate(card_df):
         date_pattern = re.compile(r'^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
         card_df = card_df.copy()
         if 'expiry_date' in card_df.columns:
             card_df['expiry_date'] = card_df['expiry_date'].astype(str).replace('/', '-', regex=True)
-            card_df = card_df.loc[card_df['expiry_date'].apply(lambda x: bool(date_pattern.match(x)) if x is not None else False)]
+            card_df = card_df.loc[card_df['expiry_date'].apply(lambda edate: bool(date_pattern.match(edate)) if edate is not None else False)]
         return card_df
-    #(card_df)
 
 
     #B_STORE_DF METHODS ONLY 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_store_continent(b_store_df):
         continents_to_keep = ['Europe', 'America', 'eeEurope', 'eeAmerica']
         b_store_df['continent'] = b_store_df['continent'].where(b_store_df['continent'].isin(continents_to_keep), None)
-        b_store_df['continent'] = b_store_df['continent'].apply(lambda x: x[2:] if x and x.startswith('ee') else x)
+        b_store_df['continent'] = b_store_df['continent'].apply(lambda con: con[2:] if con and con.startswith('ee') else con)
         return b_store_df
-    #(b_store_df)  
 
 
     #PRODS_DF METHODS ONLY
-    @staticmethod # - CLEANED (not over-engineered)
-    def process_prod_weight(prods_df):
-        prods_df['weight'] = prods_df['weight'].astype(str).str.replace('kg', '')
-        
-        def convert_weight(weight):
-            if 'g' in weight or 'ml' in weight:
-                numeric_value = pd.to_numeric(''.join(char for char in weight if char.isdigit() or char == '.'), errors='coerce') / 1000
-                return numeric_value if not pd.isna(numeric_value) else weight
-            else:
-                return pd.to_numeric(weight, errors='coerce')
+    @staticmethod
+    def convert_weight(weight):
+        if pd.isna(weight):
+            return weight
 
-        prods_df['weight'] = prods_df['weight'].apply(convert_weight)
+        if 'g' in weight or 'ml' in weight:
+            numeric_value = pd.to_numeric(''.join(char for char in weight if char.isdigit() or char == '.'), errors='coerce') / 1000
+            return numeric_value if not pd.isna(numeric_value) else weight
+        else:
+            return pd.to_numeric(weight, errors='coerce')
+
+    @staticmethod
+    def process_prod_weight(prods_df):
+        prods_df['weight'] = prods_df['weight'].str.replace('kg', '')
+        prods_df['weight'] = prods_df['weight'].apply(DataProcessor.convert_weight)
         return prods_df
     
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_col_names(prods_df):
         prods_df = prods_df.rename(columns={'Unnamed: 0': 'index'})
         prods_df = prods_df.rename(columns={'weight': 'weight(kg)'})
         prods_df = prods_df.rename(columns={'removed': 'product_status'})
         return prods_df
     
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_EAN(prods_df):
         pattern = r'^\d+$'
         prods_df = prods_df[prods_df['EAN'].astype(str).str.match(pattern)]
@@ -97,41 +102,30 @@ class DataProcessor:
     #METHODS APPLICABLE TO MORE THAN 1 DF 
     @staticmethod
     def clean_card_number(df, column_name):
-        df[column_name] = df[column_name].apply(lambda x: x if str(x).isdigit() else x)
+        df[column_name] = df[column_name].apply(lambda card_num: card_num if str(card_num).isdigit() else card_num)
         return df
-    #(card_df, 'card_number')
-    #(orders_df, 'card_number')
         
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def tonumeric_and_drop_non_numeric(df, column_names):
         for column_name in column_names:
             df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
         df.dropna(subset=column_names, how='any', inplace=True)
         return df
-    #(b_store_df, ['longitude', 'staff_numbers', 'latitude'])
-    #(orders_df 'product_quantity')
-    #(sdt_df, ['month', 'year', 'day'])
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_address(df, column_name):
-        df[column_name] = df[column_name].apply(lambda x: str(x).replace('\n', ' ') if pd.notna(x) else x)
-        return df
-    #(users_df, 'address')
-    #(b_store_df, 'address')    
+        df[column_name] = df[column_name].apply(lambda address: str(address).replace('\n', ' ') if pd.notna(address) else address)
+        return df  
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod 
     def clean_uuids(df, column_names):
         uuid_pattern = re.compile(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
         for column_name in column_names:
             if column_name in df.columns:
-                df[column_name] = df[column_name].apply(lambda x: x if uuid_pattern.match(str(x)) else x)
+                df[column_name] = df[column_name].apply(lambda uuid: uuid if uuid_pattern.match(str(uuid)) else uuid)
         return df
-    #(users_df, ['user_uuid'])
-    #(prods_df, ['uuid'])
-    #(orders_df, ['date_uuid', 'user_uuid'])
-    #(sdt_df, ['date_uuid'])
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def clean_dates(df, column_names):
         date_pattern = r'^\d{4}-\d{2}-\d{2}$'
         for column_name in column_names:
@@ -142,40 +136,20 @@ class DataProcessor:
                 df.loc[non_conforming_mask, column_name] = pd.NaT
         df = df.dropna(subset=column_names)
         return df    
-    #(users_df, ['join_date', 'date_of_birth'])
-    #(card_df, ['date_payment_confirmed'])
-    #(b_store_df, ['opening_date'])
-    #(prods_df, ['date_added'])
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod
     def drop_df_cols(df, column_names):
         df.drop(columns=column_names, inplace=True)
         return df
-    #(card_df, ['card_number expiry_date', 'Unnamed: 0'])
-    #(b_store_df, ['lat'])
-    #(orders_df, ['level_0', 'first_name', 'last_name', '1'])
     
-    #method to drop duplicates in df
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod 
     def drop_duplicates(df):
         df = df.copy()
         df = df.drop_duplicates()
         return df
-    #(users_df)
-    #(card_df)
-    #(b_store_df)
-    #(prods_df)
-    #(orders_df)
-    #(sdt_df)
 
-    @staticmethod # - CLEANED (not over-engineered)
+    @staticmethod 
     def fix_index(df, index_col):
         df.reset_index(drop=True, inplace=True)
         df.loc[:, index_col] = range(1, len(df) + 1)
         return df
-    #(users_df, 'index')
-    #(b_store_df, 'index')
-    #(prods_df, 'index')
-    #(orders_df, 'index')
-            
-#all methods are static to avoid contantly having to create instances for each df in order to use the methods from this class
--- database_utils.py
+++ database_utils.py
@@ -1,34 +1,58 @@
-import yaml
 from sqlalchemy import create_engine, inspect
+import yaml
 
 
 class DatabaseConnector:
+
+    '''
+    DataConnector Class
+    -------
+    This script creates the engine for the connection to the database and uploads the DataFrames to the database once data is transformed.
+    The methods included are mostly public methods for code reusablity.
+
+    Methods
+    -------
+        * __init__ - a magic method which initializes the engine created and path to the credentials file
+        * _read_db_creds - a private method to read in the credentials associated with creating the engine
+        * _init_db_engine - a private method which initializes the engine with the relvant credentials 
+        * list_db_tables - a public method which retrieves only the table names from an RDS database
+        * _get_local_db_engine - a private method which specifies the local database credentials path to create the engine to upload the transformed data to the database
+        * _upload_to_db - a private method which uploads transformed data to sales_data database and notifies user of upload success
+    '''
+        
     def __init__(self, creds_path):
         self.creds_path = creds_path
         self.engine = self.init_db_engine()
 
-    def read_db_creds(self):
+
+    def _read_db_creds(self):
         with open(self.creds_path, 'r') as creds:
             credentials = yaml.safe_load(creds)
         return dict(credentials)
     
-    def init_db_engine(self):
+    
+    def _init_db_engine(self):
         credentials = self.read_db_creds()
         db_url = f"postgresql://{credentials['RDS_USER']}:{credentials['RDS_PASSWORD']}@{credentials['RDS_HOST']}:{credentials['RDS_PORT']}/{credentials['RDS_DATABASE']}"
         engine = create_engine(db_url)
         return engine    
 
+
     def list_db_tables(self):
         inspector = inspect(self.engine)
         table_names = inspector.get_table_names()
         return table_names   
+    
 
-    def upload_to_db(self, df, table_name, index=False, if_exists='replace'):
+    #UPLOAD TO DB
+    def _get_local_db_engine(self):
         with open('/Users/itsanya/AiCore/MRDC/sales_data_creds.yaml', 'r') as ldbcreds:
             ldb_creds = yaml.safe_load(ldbcreds)
 
         ldb_connect = f"postgresql://{ldb_creds['username']}:{ldb_creds['password']}@{ldb_creds['host']}:{ldb_creds['port']}/{ldb_creds['database']}"
-        ldb_engine = create_engine(ldb_connect)
+        return create_engine(ldb_connect)
 
+    def _upload_to_db(self, df, table_name, index=False, if_exists='replace'):
+        ldb_engine = self.get_local_db_engine()
         df.to_sql(name=table_name, con=ldb_engine, index=index, if_exists=if_exists)
         print(f"Data uploaded to '{table_name}' table successfully.")
